2a5a328bdf59263b678c558cd06779c2cd3351d7
Dmitry Vasilev <dmitry@t3dev.ru>
2022-07-29 16:21:53 +0300

    [TASK] - add screens as preview

---
+++ templates/partials/informers_columns.tmpl.php
@@ -0,0 +1,224 @@
+<?php
+require_once('Classes/AbstractClass.php');
+$abstractData = new AbstractClass();
+$cities = $abstractData->getDomain() . 'cities.php';
+?>
+
+<div class="wrapper">
+    <p class="otst"><br></p>
+    <h4>Информер № 1</h4>
+    <p><br /></p>
+
+    <div class="row">
+        <div class="col">
+            <h5>Classic</h5>
+            <div class="wrap">
+                <img src="assets/screens/inf1-classic.png" />
+            </div>
+        </div>
+        <div class="col">
+            <h5>High-Tech</h5>
+            <div class="wrap">
+                <img src="assets/screens/inf1-ht.png" />
+            </div>
+        </div>
+        <div class="col">
+            <h5>Neon</h5>
+            <div class="wrap">
+                <img src="assets/screens/inf1-neon.png" />
+            </div>
+        </div>
+    </div>
+
+    <p class="otst"><br></p>
+    <h4>Информер № 2</h4>
+    <p><br /></p>
+    <div class="row">
+        <div class="col">
+            <h5>Classic</h5>
+            <div class="wrap wrap2">
+                <img src="assets/screens/inf2-classic.png" />
+            </div>
+        </div>
+        <div class="col">
+            <h5>High-Tech</h5>
+            <div class="wrap wrap2">
+                <img src="assets/screens/inf2-ht.png" />
+            </div>
+        </div>
+        <div class="col">
+            <h5>Neon</h5>
+            <div class="wrap wrap2">
+                <img src="assets/screens/inf2-neon.png" />
+            </div>
+        </div>
+    </div>
+
+    <p class="otst"><br></p>
+    <h4>Информер № 3</h4>
+    <p><br /></p>
+    <div class="row">
+        <div class="col">
+            <h5>Classic</h5>
+            <div class="wrap wrap2">
+                <img src="assets/screens/inf3-classic.png" />
+            </div>
+        </div>
+        <div class="col">
+            <h5>High-Tech</h5>
+            <div class="wrap wrap2">
+                <img src="assets/screens/inf3-ht.png" />
+            </div>
+        </div>
+        <div class="col">
+            <h5>Neon</h5>
+            <div class="wrap wrap2">
+                <img src="assets/screens/inf3-neon.png" />
+            </div>
+        </div>
+    </div>
+
+    <p class="otst"><br></p>
+    <h4>Информер № 4</h4>
+    <p><br /></p>
+    <div class="row">
+        <div class="col">
+            <h5>Classic</h5>
+            <div class="wrap wrap2">
+                <img src="assets/screens/inf4-classic.png" />
+            </div>
+        </div>
+        <div class="col">
+            <h5>High-Tech</h5>
+            <div class="wrap wrap2">
+                <img src="assets/screens/inf4-ht.png" />
+            </div>
+        </div>
+        <div class="col">
+            <h5>Neon</h5>
+            <div class="wrap wrap2">
+                <img src="assets/screens/inf4-neon.png" />
+            </div>
+        </div>
+    </div>
+
+    <p class="otst"><br></p>
+    <h4>Информер № 5</h4>
+    <p><br /></p>
+    <div class="row">
+        <div class="col">
+            <h5>Classic</h5>
+            <div class="wrap wrap2">
+                <img src="assets/screens/inf5-classic.png" />
+            </div>
+        </div>
+        <div class="col">
+            <h5>High-Tech</h5>
+            <div class="wrap wrap2">
+                <img src="assets/screens/inf5-ht.png" />
+            </div>
+        </div>
+        <div class="col">
+            <h5>Neon</h5>
+            <div class="wrap wrap2">
+                <img src="assets/screens/inf5-neon.png" />
+            </div>
+        </div>
+    </div>
+
+    <p class="otst"><br></p>
+    <h4>Информер № 6</h4>
+    <p><br /></p>
+    <div class="row">
+        <div class="col">
+            <h5>Classic</h5>
+            <div class="wrap wrap2">
+                <img src="assets/screens/inf6-classic.png" />
+            </div>
+        </div>
+        <div class="col">
+            <h5>High-Tech</h5>
+            <div class="wrap wrap2">
+                <img src="assets/screens/inf6-ht.png" />
+            </div>
+        </div>
+        <div class="col">
+            <h5>Neon</h5>
+            <div class="wrap wrap2">
+                <img src="assets/screens/inf6-neon.png" />
+            </div>
+        </div>
+    </div>
+
+    <p class="otst"><br></p>
+    <h4>Информер № 7</h4>
+    <p><br /></p>
+    <div class="row">
+        <div class="col">
+            <h5>Classic</h5>
+            <div class="wrap wrap2">
+                <img src="assets/screens/inf7-classic.png" />
+            </div>
+        </div>
+        <div class="col">
+            <h5>High-Tech</h5>
+            <div class="wrap wrap2">
+                <img src="assets/screens/inf7-ht.png" />
+            </div>
+        </div>
+        <div class="col">
+            <h5>Neon</h5>
+            <div class="wrap wrap2">
+                <img src="assets/screens/inf7-neon.png" />
+            </div>
+        </div>
+    </div>
+
+    <p class="otst"><br></p>
+    <h4>Информер № 8</h4>
+    <p><br /></p>
+    <div class="row">
+        <div class="col">
+            <h5>Classic</h5>
+            <div class="wrap wrap2">
+                <img src="assets/screens/inf8-classic.png" />
+            </div>
+        </div>
+        <div class="col">
+            <h5>High-Tech</h5>
+            <div class="wrap wrap2">
+                <img src="assets/screens/inf8-ht.png" />
+            </div>
+        </div>
+        <div class="col">
+            <h5>Neon</h5>
+            <div class="wrap wrap2">
+                <img src="assets/screens/inf8-neon.png" />
+            </div>
+        </div>
+    </div>
+
+    <p class="otst"><br></p>
+    <h4>Информер № 9</h4>
+    <p><br /></p>
+    <div class="row">
+        <div class="col">
+            <h5>Classic</h5>
+            <div class="wrap wrap2">
+                <img src="assets/screens/inf9-classic.png" />
+            </div>
+        </div>
+        <div class="col">
+            <h5>High-Tech</h5>
+            <div class="wrap wrap2">
+                <img src="assets/screens/inf9-ht.png" />
+            </div>
+        </div>
+        <div class="col">
+            <h5>Neon</h5>
+            <div class="wrap wrap2">
+                <img src="assets/screens/inf9-neon.png" />
+            </div>
+        </div>
+    </div>
+</div>
