73de2174d24f624c0b116a335ecacb61e10f38e7
Dmitry Vasilev <dmitry@t3dev.ru>
2022-08-03 23:00:33 +0300

    [TASK] - weather_1 in table style

---
+++ templates/classic/_weather_1_back.tmpl.php
@@ -0,0 +1,89 @@
+<?php
+    foreach ($objects['0'] as $forecast) {
+        $forecatsArr[] = $forecast;
+    }
+if ($_REQUEST['transpar'] == 1) {?>
+    <style>
+        .view-1-classic-longcustom {
+            background-image: url("<?php echo $abstractData->getBgWeatherIconSrc($object, 'png', 'longcustom') ?>");
+            background-size:100% 100%;
+        }
+        .view-1-classic-custom {
+            background-image: url("<?php echo $abstractData->getBgWeatherIconSrc($object, 'png', 'custom') ?>");
+            background-size: cover;
+            background-position: center;
+        }
+    </style>
+<?php }?>
+
+<body>
+<section class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-container">
+<div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>" >
+
+    <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-content">
+            <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-city-day <?php if ($_REQUEST['transpar'] == 1) {
+    echo 'view-1-classic-custom';
+} ?>">
+                <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-city-day-info-container">
+                    <p class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-city-day-info"><?php echo $object['name'] ?></p>
+                    <p class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-city-day-info"><?php echo $abstractData->getWeek() ?>,</p>
+                    <p class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-city-day-info"><?php echo $abstractData->getDate('d.m.Y') ?></p>
+                </div>
+            </div>
+
+            <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-maincontent
+                <?php if ($_REQUEST['transpar'] == 1) {
+    echo 'view-1-classic-longcustom';
+} ?>">
+                <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-weather"><?php echo $abstractData->getWeatherDescription($object['icon']) ?></div>
+                <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-weather-icon"></div>
+                <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-dergees-cels">
+                    <?php echo $object['tempC'] ?>°
+                </div>
+                <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-dergees-far">
+                    <?php echo $object['tempF'] ?>F
+                </div>
+                <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-typeofwind">
+                    <?php echo $abstractData->getWwindDirection($object['wd']) ?>
+                </div>
+                <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-windspeed">
+                    <?php echo $abstractData->getMetersPerSecond($object['ws']) ?> м/с
+                </div>
+                <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-degrees">
+                    <?php echo $object['gmtMin'] ?>°
+                </div>
+                <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-wet">
+                    Влажность: <?php echo $object['rh'] ?>%
+                </div>
+                <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-pressure">
+                    Давление: <?php echo $abstractData->getMillimetersOfMercury($object['psl']) ?> мм. рт. ст.
+                </div>
+            </div>
+            <?php for ($i=0; $i < 3; $i++) {
+    if ($i == 0) {
+        $className = 'morning';
+    }
+    if ($i == 1) {
+        $className = 'day';
+    }
+    if ($i == 2) {
+        $className = 'evening';
+    } ?>
+                <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-<?php echo $className ?>" >
+                    <p class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-timeofday" >
+                        <?php echo $abstractData->getTimesOfDay($abstractData->getDateFromString($forecatsArr[$i]['dt'], 'H:i')) ?>
+                    </p>
+                    <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-timeofday-icon">
+                        <?php echo $abstractData->getWeatherIcon($forecatsArr[$i]) ?>
+                    </div>
+                    <p class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-temp">
+                        <?php echo $forecatsArr[$i]['tempC']; ?>°
+                    </p>
+                </div>
+            <?php
+}?>
+        </div>
+    </div>
+
+</section>
+</body>
--- templates/classic/weather_1.tmpl.php
+++ templates/classic/weather_1.tmpl.php
@@ -2,6 +2,9 @@
     foreach ($objects['0'] as $forecast) {
         $forecatsArr[] = $forecast;
     }
+
+    $time = date('H:i', time());
+
 if ($_REQUEST['transpar'] == 1) {?>
     <style>
         .view-1-classic-longcustom {
@@ -17,73 +20,128 @@ if ($_REQUEST['transpar'] == 1) {?>
 <?php }?>
 
 <body>
-<section class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-container">
-<div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>" >
+<table class="informers1" id="fon_table">
+    <tbody>
+    <tr>
+        <td class="td-blue ">
+            <h3><?php echo $object['name'] ?> <?php echo $abstractData->getWeek() ?>, <?php echo $abstractData->getDate('d.m.Y') ?></h3>
+        </td>
+    </tr>
+    <tr>
+        <td class="td-blue">
 
-    <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-content">
-            <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-city-day <?php if ($_REQUEST['transpar'] == 1) {
-    echo 'view-1-classic-custom';
-} ?>">
-                <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-city-day-info-container">
-                    <p class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-city-day-info"><?php echo $object['name'] ?></p>
-                    <p class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-city-day-info"><?php echo $abstractData->getWeek() ?>,</p>
-                    <p class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-city-day-info"><?php echo $abstractData->getDate('d.m.Y') ?></p>
-                </div>
-            </div>
+        </td>
+    </tr>
+    <tr>
+        <td class="td-blue">
+            <?php echo $abstractData->getTimesOfDay($time) ?>
+        </td>
+    </tr>
 
-            <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-maincontent
-                <?php if ($_REQUEST['transpar'] == 1) {
-    echo 'view-1-classic-longcustom';
-} ?>">
-                <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-weather"><?php echo $abstractData->getWeatherDescription($object['icon']) ?></div>
-                <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-weather-icon"></div>
-                <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-dergees-cels">
-                    <?php echo $object['tempC'] ?>°
-                </div>
-                <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-dergees-far">
-                    <?php echo $object['tempF'] ?>F
-                </div>
-                <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-typeofwind">
-                    <?php echo $abstractData->getWwindDirection($object['wd']) ?>
-                </div>
-                <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-windspeed">
-                    <?php echo $abstractData->getMetersPerSecond($object['ws']) ?> м/с
-                </div>
-                <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-degrees">
-                    <?php echo $object['gmtMin'] ?>°
-                </div>
-                <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-wet">
-                    Влажность: <?php echo $object['rh'] ?>%
-                </div>
-                <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-pressure">
-                    Давление: <?php echo $abstractData->getMillimetersOfMercury($object['psl']) ?> мм. рт. ст.
-                </div>
-            </div>
-            <?php for ($i=0; $i < 3; $i++) {
-    if ($i == 0) {
-        $className = 'morning';
-    }
-    if ($i == 1) {
-        $className = 'day';
-    }
-    if ($i == 2) {
-        $className = 'evening';
-    } ?>
-                <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-<?php echo $className ?>" >
-                    <p class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-timeofday" >
-                        <?php echo $abstractData->getTimesOfDay($abstractData->getDateFromString($forecatsArr[$i]['dt'], 'H:i')) ?>
-                    </p>
-                    <div class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-timeofday-icon">
-                        <?php echo $abstractData->getWeatherIcon($forecatsArr[$i]) ?>
-                    </div>
-                    <p class="view-1-<?php echo $_REQUEST['weather_tip_img']?>-temp">
-                        <?php echo $forecatsArr[$i]['tempC']; ?>°
-                    </p>
-                </div>
-            <?php
-}?>
-        </div>
-    </div>
+    <tr>
+        <td class="td-blue" >
+            <p><?php echo $abstractData->getWeatherDescription($object['icon']) ?></p>
+        </td>
+    </tr>
 
-</section>
+    <tr>
+        <td class="td-blue td-icons" >
+            <?php echo $abstractData->getWeatherIcon($object, '', '', 'max-width: 65%;') ?>
+        </td>
+    </tr>
+    <tr>
+        <td class="td-blue temperature">
+            <?php echo $object['tempC'] ?>°
+        </td>
+    </tr>
+    <tr>
+        <td class="td-blue">
+            <?php echo $object['tempF'] ?>F
+        </td>
+    </tr>
+
+    <tr>
+        <td class="td-blue td-icons" >
+            <img src="<?php echo $abstractData->getDomain(). 'assets/classic/icons/wind-route-2.svg' ?>" style="max-width: 45%;" alt="">
+        </td>
+    </tr>
+    <tr>
+        <td class="td-blue td-text" >
+            <p> <?php echo $abstractData->getWwindDirection($object['wd']) ?></p>
+        </td>
+    </tr>
+    <tr>
+        <td class="td-blue td-icons" >
+            <img src="<?php echo $abstractData->getDomain(). 'assets/classic/icons/speed-2.svg' ?>" style="max-width: 65%;" alt="">
+        </td>
+    </tr>
+    <tr>
+        <td class="td-blue td-text" >
+            <p><?php echo $abstractData->getMetersPerSecond($object['ws']) ?> м/с </p>
+        </td>
+    </tr>
+
+    <tr>
+        <td class="td-blue td-icons" >
+            <img src="<?php echo $abstractData->getDomain(). 'assets/classic/icons/grade.svg' ?>" style="max-width: 45%;" alt="">
+        </td>
+    </tr>
+    <tr>
+        <td class="td-blue td-text" >
+            <p><?php echo $object['gmtMin'] ?>°</p>
+        </td>
+    </tr>
+    <tr>
+        <td class="td-blue td-icons" >
+            <img src="<?php echo $abstractData->getDomain(). 'assets/classic/icons/wet-2.svg' ?>" style="max-width: 45%;" alt="">
+        </td>
+    </tr>
+    <tr>
+        <td class="td-blue td-text" >
+            <p>Влажность: <?php echo $object['rh'] ?>%</p>
+        </td>
+    </tr>
+    <tr>
+        <td class="td-blue td-icons" >
+            <img src="<?php echo $abstractData->getDomain(). 'assets/classic/icons/pressure.svg' ?>" style="max-width: 45%;" alt="">
+        </td>
+    </tr>
+
+    <tr>
+        <td class="td-blue td-text" >
+            <p> Давление: <?php echo $abstractData->getMillimetersOfMercury($object['psl']) ?> мм. рт. ст.</p>
+        </td>
+    </tr>
+
+
+    <?php for ($i=0; $i < 3; $i++) {
+        if ($i == 0) {
+            $className = 'morning';
+        }
+        if ($i == 1) {
+            $className = 'day';
+        }
+        if ($i == 2) {
+            $className = 'evening';
+        }
+        ?>
+        <tr>
+            <td class="td-blue">
+                <?php echo $abstractData->getTimesOfDay($abstractData->getDateFromString($forecatsArr[$i]['dt'], 'H:i')) ?>
+            </td>
+        </tr>
+        <tr>
+            <td class="td-blue" >
+                <?php echo $abstractData->getWeatherIcon($forecatsArr[$i], '', '', 'max-width: 45%;') ?>
+            </td>
+        </tr>
+        <tr>
+            <td class="td-blue">
+                <p class="tempo"> <?php echo $forecatsArr[$i]['tempC']; ?>°</p>
+            </td>
+        </tr>
+        <?php
+    }?>
+    </tbody>
+</table>
 </body>
