2f372345e4caa574e8cc547add28a5ab0dbce6f1
Dmitry Vasilev <dmitry@t3dev.ru>
2022-08-13 17:22:23 +0200

    Changes on server

--- templates/partials/code_informer_form.tmpl.php
+++ templates/partials/code_informer_form.tmpl.php
@@ -1,6 +1,6 @@
 <div class="code_informer">
     <form action="" style="text-align:center; margin-top: 20px;">
-        <textarea name='code_informer' style='width: 550px; height: 90px; padding: 15px; line-height: 1.4em; overflow: hidden;'><iframe src="<?php echo $abstractData->generateUrl($requestArray); ?>"></iframe></textarea>
+        <textarea name='code_informer' style='width: 550px; height: 90px; padding: 15px; line-height: 1.4em; overflow: hidden;'><iframe src="<?php echo $abstractData->generateUrl($requestArray); ?>" width="100%" height="100%"></iframe></textarea>
     </form>
     <p><br /></p>
     <p style="font-style:italic; font-weight:bold;" align="center"> Скопируйте код информера и вставьте в блок слайда типа "HTML"</p>
